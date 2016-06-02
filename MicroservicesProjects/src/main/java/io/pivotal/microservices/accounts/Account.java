befa34f693a47d6b1b6782e5513885b748b29683
dschuld <davidschuld@gmail.com>
2016-06-02 11:18:14 +0200

    Added missing imports to Account entity

--- MicroservicesProjects/src/main/java/io/pivotal/microservices/accounts/Account.java
+++ MicroservicesProjects/src/main/java/io/pivotal/microservices/accounts/Account.java
@@ -3,6 +3,11 @@ package io.pivotal.microservices.accounts;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Persistent account entity with JPA markup. Accounts are stored in an H2
  * relational database.
@@ -13,92 +18,92 @@ import java.math.BigDecimal;
 @Table(name = "T_ACCOUNT")
 public class Account implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public static Long nextId = 0L;
+    public static Long nextId = 0L;
 
-	@Id
-	protected Long id;
+    @Id
+    protected Long id;
 
-	protected String number;
+    protected String number;
 
-	@Column(name = "name")
-	protected String owner;
+    @Column(name = "name")
+    protected String owner;
 
-	protected BigDecimal balance;
+    protected BigDecimal balance;
 
-	/**
-	 * This is a very simple, and non-scalable solution to generating unique
-	 * ids. Not recommended for a real application. Consider using the
-	 * <tt>@GeneratedValue</tt> annotation and a sequence to generate ids.
-	 * 
-	 * @return The next available id.
-	 */
-	protected static Long getNextId() {
-		synchronized (nextId) {
-			return nextId++;
-		}
+    /**
+     * This is a very simple, and non-scalable solution to generating unique
+     * ids. Not recommended for a real application. Consider using the
+     * <tt>@GeneratedValue</tt> annotation and a sequence to generate ids.
+     * 
+     * @return The next available id.
+     */
+    protected static Long getNextId() {
+	synchronized (nextId) {
+	    return nextId++;
 	}
+    }
 
-	/**
-	 * Default constructor for JPA only.
-	 */
-	protected Account() {
-		balance = BigDecimal.ZERO;
-	}
+    /**
+     * Default constructor for JPA only.
+     */
+    protected Account() {
+	balance = BigDecimal.ZERO;
+    }
 
-	public Account(String number, String owner) {
-		id = getNextId();
-		this.number = number;
-		this.owner = owner;
-		balance = BigDecimal.ZERO;
-	}
+    public Account(String number, String owner) {
+	id = getNextId();
+	this.number = number;
+	this.owner = owner;
+	balance = BigDecimal.ZERO;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+	return id;
+    }
 
-	/**
-	 * Set JPA id - for testing and JPA only. Not intended for normal use.
-	 * 
-	 * @param id
-	 *            The new id.
-	 */
-	protected void setId(long id) {
-		this.id = id;
-	}
+    /**
+     * Set JPA id - for testing and JPA only. Not intended for normal use.
+     * 
+     * @param id
+     *            The new id.
+     */
+    protected void setId(long id) {
+	this.id = id;
+    }
 
-	public String getNumber() {
-		return number;
-	}
+    public String getNumber() {
+	return number;
+    }
 
-	protected void setNumber(String accountNumber) {
-		this.number = accountNumber;
-	}
+    protected void setNumber(String accountNumber) {
+	this.number = accountNumber;
+    }
 
-	public String getOwner() {
-		return owner;
-	}
+    public String getOwner() {
+	return owner;
+    }
 
-	protected void setOwner(String owner) {
-		this.owner = owner;
-	}
+    protected void setOwner(String owner) {
+	this.owner = owner;
+    }
 
-	public BigDecimal getBalance() {
-		return balance.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-	}
+    public BigDecimal getBalance() {
+	return balance.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+    }
 
-	public void withdraw(BigDecimal amount) {
-		balance.subtract(amount);
-	}
+    public void withdraw(BigDecimal amount) {
+	balance.subtract(amount);
+    }
 
-	public void deposit(BigDecimal amount) {
-		balance.add(amount);
-	}
+    public void deposit(BigDecimal amount) {
+	balance.add(amount);
+    }
 
-	@Override
-	public String toString() {
-		return number + " [" + owner + "]: $" + balance;
-	}
+    @Override
+    public String toString() {
+	return number + " [" + owner + "]: $" + balance;
+    }
 
 }
