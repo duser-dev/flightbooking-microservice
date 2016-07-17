5bfeff6198d1b7e9b03fe728e9367b73dac1eeb9
Daniel Beyer <Daniel.Beyer@cgi.com>
2016-07-17 12:18:37 +0200

    added imports

--- microservices-accounts/src/main/java/com/cgi/microservices/league/exception/LeagueNotFoundException.java
+++ microservices-accounts/src/main/java/com/cgi/microservices/league/exception/LeagueNotFoundException.java
@@ -1,5 +1,8 @@
 package com.cgi.microservices.league.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
 /**
  * Allow the controller to return a 404 if an account is not found by simply
  * throwing this exception. The @ResponseStatus causes Spring MVC to return a
