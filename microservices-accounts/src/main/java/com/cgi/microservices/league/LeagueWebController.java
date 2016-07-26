9cf1f10295cbf8d0983cb4255d081d4cf9880501
Daniel Beyer <Daniel.Beyer@cgi.com>
2016-07-26 20:01:34 +0200

    teststand

--- microservices-accounts/src/main/java/com/cgi/microservices/league/LeagueWebController.java
+++ microservices-accounts/src/main/java/com/cgi/microservices/league/LeagueWebController.java
@@ -4,73 +4,82 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * League Web Controller which is responsible for handling the Json Answers and
- * render a page.
- * 
+ * League Web Controller
+ * He is resposible for handling the Json Answers and render
+ * a page.
  * @author beyerd
  *
  */
 @Controller
-@EnableFeignClients
 public class LeagueWebController {
-
+	
 	@Autowired
-	protected LeaguedataServiceClient leaguedataService;
-
+	protected WebLeagueService leagueService;
 	protected Logger logger = Logger.getLogger(LeagueWebController.class.getName());
-
+	
 	/**
+	 * Constructor...
+	 * @param accountsService
+	 */
+//	public LeagueWebController(WebLeagueService leagueService) {
+//		logger.info("[LeagueWebController] - succesfully reached service...");
+//		this.leagueService = leagueService;
+//	}
+	
+	/** 
 	 * Default constructor
-	 * 
 	 * @param model
 	 * @return
 	 */
 	public LeagueWebController() {
 		logger.info("[LeagueWebController] - reached default constructor...");
+		this.leagueService  = null;
 	}
-
+	
 	@RequestMapping("/")
 	public String home() {
 		logger.info("[LeagueWebController] - called index ...");
 		return "index";
 	}
-
+	
 	@RequestMapping("/league/{leagueYear}")
 	public String leagueByYear(Model model, @PathVariable("leagueYear") String leagueYear) {
 		logger.info("[LeagueWebController] - called Method leagueByYear(model) ...");
-		// League league = leagueService.findByYear(leagueYear);
-
-		// logger.info("web-service byOwner() found: " + league);
-		// model.addAttribute("search", name);
-		// if (league != null) {
-		// logger.info("...we have accounts...");
-		// model.addAttribute("leagues", league);
-		// }
-
-		// return "accounts";
+		League league = leagueService.findByYear(leagueYear); 
+				
+		logger.info("web-service byOwner() found: " + league);
+		
+		model.addAttribute("search", name);
+		
+		if (league != null) {
+			logger.info("...we have accounts...");
+			model.addAttribute("leagues", league);
+		}
+			
+		//return "accounts";
 		return "summary";
 	}
-
+	
 	@RequestMapping("/teams/{leagueYear}")
 	public String teamsByYear(Model model, @PathVariable("leagueYear") String leagueYear) {
 		logger.info("[LeagueWebController] - called Method teamsByYear(model) ...");
-		List<Team> teamList = leaguedataService.getTeams(Integer.valueOf(leagueYear));
-
-		logger.info("[LeagueWebController] - web-service byOwner() found: " + teamList);
-
-		if (teamList != null) {
+		League league = leagueService.findByYear(leagueYear); 
+		
+		logger.info("[LeagueWebController] - web-service byOwner() found: " + league);
+		
+		if (league != null) {
 			logger.info("...we have accounts...");
-			model.addAttribute("teams", teamList);
+			model.addAttribute("league", league);
 		}
-
+			
 		return "summary";
 	}
-
+	
+	
 }
