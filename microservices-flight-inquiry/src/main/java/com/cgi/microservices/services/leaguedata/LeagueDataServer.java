a8c5ca2b83dc1ec7a617b2b18ce5bb6a30553342
Daniel Beyer <Daniel.Beyer@cgi.com>
2016-08-03 19:14:54 +0200

    hyterix eingebunden

--- microservices-flight-inquiry/src/main/java/com/cgi/microservices/services/leaguedata/LeagueDataServer.java
+++ microservices-flight-inquiry/src/main/java/com/cgi/microservices/services/leaguedata/LeagueDataServer.java
@@ -3,10 +3,12 @@ package com.cgi.microservices.services.leaguedata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class LeagueDataServer {
 
